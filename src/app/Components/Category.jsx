11e7fb2e622545537f5d74834c2f7e7aa99ae4c7
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2025-07-23 21:56:54 +0530

    homw

---
+++ src/app/Components/Category.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+const Category = () => {
+    return (
+        <div>Category</div>
+    )
+}
+
+export default Category
--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Category from '../Components/Category'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+      <Category />
+    </>
   )
 }
 
