da8b3f18e5fbba3dbc62b997c2d37cf25b8d661e
piyushbishnoi515 <piyushbishnoi29129@gmail.com>
2025-07-23 21:37:11 +0530

    first

---
+++ src/app/Components/Ourclient.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Ourclient = () => {
+    return (
+        <div className='text-center text'>
+
+            <h1 className='text-4xl font-bold text-gray-800 mb-4'>Our Clients</h1>
+            <p className='text-lg text-gray-600 mb-8'>We are proud to work with a diverse range of clients across various industries.</p>
+
+        </div>
+    )
+}
+
+export default Ourclient
